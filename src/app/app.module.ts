8407ffcfc595e3d189732fcf276fb34b909ee6eb
pkurochka <p.kurochka@svitla.com>
2018-03-09 14:33:50 +0200

    build(dependencies): Add project main dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './materal/material.module';
 
 import { AppComponent } from './app.component';
+import { reducers, metaReducers } from './reducers';
+import { AppEffects } from './effects/app.effects';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -12,7 +21,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects]),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/materal/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const MODULES = [
+
+];
+
+@NgModule({
+  imports: MODULES,
+  exports: MODULES
+})
+export class MaterialModule { }
